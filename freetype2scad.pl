17bbb37427c2fec5fdcca589cd98aaaee0aac21c
James Mastros <james@mastros.biz>
2010-11-17 23:58:37 +0000

    render as a polygon, rather then attempting to stroke the outline

--- freetype2scad.pl
+++ freetype2scad.pl
@@ -3,10 +3,12 @@ use warnings;
 use strict;
 use Font::FreeType;
 use charnames ();
+use Data::Dump::Streamer;
 
-# LIMITATIONS: No support for right-to-left characters.
+# LIMITATIONS:
+#  - No support for right-to-left characters.
 
-my $face = Font::FreeType->new->face('/usr/share/fonts/truetype/msttcorefonts/arial.ttf');
+my $face = Font::FreeType->new->face(shift || '/usr/share/fonts/truetype/msttcorefonts/arial.ttf');
 # In points.
 my $nominal_size = 8;
 # In DPI.
@@ -27,46 +29,78 @@ $face->foreach_char(sub {
                       my $module_name = charnames::viacode($glyph->char_code) || $glyph->name;
                       $module_name =~ s/ /_/g;
                       $module_name =~ s/-/_/g;
-                      print "module $module_name(stroke_width) {\n";
+                      print "module $module_name() {\n";
+
+                      my ($state) = {};
 
                       $glyph->outline_decompose(
-                                                move_to => \&move_to,
-                                                line_to => \&line_to,
-                                                cubic_to => \&cubic_to,
+                                                move_to => sub {move_to($state, @_)},
+                                                line_to => sub {line_to($state, @_)},
+                                                cubic_to => sub {cubic_to($state, @_)},
                                                );
+
+                      move_to($state, 0, 0);
+
+                      # Dump $state;
+
+                      if ($state->{paths}) {
+                        print " polygon(points=[\n";
+                        my $n = 0;
+                        for my $point (sort {$state->{coded_points}{$a} <=> $state->{coded_points}{$b}} keys %{$state->{coded_points}}) {
+                          print "   [$point], // $state->{coded_points}{$point}\n";
+                          $n++;
+                        }
+                        print "  ],\n";
+                        print "  paths = [\n";
+                        for my $path (@{$state->{paths}}) {
+                          print "   [", join(", ", @$path), "],\n";
+                        }
+                        print "  ]\n";
+                        
+                        print " );\n";
+                      }
+
                       print " translate([", $glyph->horizontal_advance, ", 0, 0]) child(0);\n";
                       print "}\n\n";
 });
 
-my ($last_x, $last_y);
+sub assign_coded_point {
+  my ($state, $x, $y) = @_;
+
+  #print "//assign_coded_point(..., $x, $y)\n";
+  my $str = "$x, $y";
+  $state->{n_coded_points} //= 0;
+  if (not exists $state->{coded_points}{$str}) {
+    $state->{coded_points}{$str} = $state->{n_coded_points}++;
+  }
+  #print "//assign_coded_point(..., $x, $y) -> $state->{coded_points}{$str}\n";
+
+  return $state->{coded_points}{$str};
+}
 
 sub move_to {
-  my ($x, $y) = @_;
-  # print " // move_to $x, $y\n";
-  ($last_x, $last_y) = ($x, $y);
+  my ($state, $x, $y) = @_;
+  #print " // move_to ($x, $y)\n";
+
+  if ($state->{current_path}) {
+    push @{$state->{paths}}, $state->{current_path};
+    $state->{current_path} = [];
+  }
+
+  $state->{current_pos} = assign_coded_point($state, $x, $y);
 }
 
 sub line_to {
-  my ($x, $y) = @_;
-  print "  // line ($last_x, $last_y) - ($x, $y)\n";
-  my ($delta_x) = $x - $last_x;
-  my ($delta_y) = $y - $last_y;
-  print "  // delta ($delta_x, $delta_y)\n";
-  my $angle = atan2($delta_y, $delta_x) * 360/(2*$PI);
-  print "  // angle $angle\n";
-  my $length = sqrt($delta_x**2 + $delta_y**2);
-  print "  // length $length\n";
-  
-  my $stroke = "translate([-$length, -stroke_width]) square([$length, stroke_width])";
-  print "  translate([$x, $y]) rotate(v=[0,0,1], a=$angle) $stroke;\n";
-  
-  ($last_x, $last_y) = ($x, $y);
+  my ($state, $x, $y) = @_;
+  #print "  // line_to ($x, $y)\n";
+
+  push @{$state->{current_path}}, assign_coded_point($state, $x, $y);
+
+  $state->{current_pos} = assign_coded_point($state, $x, $y);
 }
 
 sub cubic_to {
-  my ($destx, $desty, $c1x, $c1y, $c2x, $c2y) = @_;
-  print "// cubic_to ($destx, $desty) control 1 ($c1x, $c1y) control 2 ($c2x, $c2y)\n";
-  line_to($destx, $desty);
-  
-  ($last_x, $last_y) = ($destx, $desty);
+  my ($state, $destx, $desty, $c1x, $c1y, $c2x, $c2y) = @_;
+  # print "// cubic_to ($destx, $desty) control 1 ($c1x, $c1y) control 2 ($c2x, $c2y)\n";
+  line_to($state, $destx, $desty);
 }
